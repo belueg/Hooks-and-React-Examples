139104f114dc34e93753b3ec020b50ba6907637a
Belén González <belg1294@gmail.com>
2020-09-24 16:56:30 +0200

    Testing useEffect Hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState, useEffect} from "react";
+import './App.css'
 function App() {
+
+
+  const [count, setCount] = useState(0)
+const [title, setTitle] = useState("React")
+
+  useEffect(() => {
+    console.log("Title changed")
+  
+  }, [title])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<p onMouseOver={()=> setTitle("User")}>Helloo {title}</p>
+<button onClick={() =>  setCount(count + 1 )}>{count}</button>
     </div>
   );
 }
